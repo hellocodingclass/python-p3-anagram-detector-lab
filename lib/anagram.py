a75600032f1f28e92554dcfd3748827255569fcb
hellocodingclass <132718779+hellocodingclass@users.noreply.github.com>
2023-07-28 12:54:40 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, anagram):
+        self.anagram=anagram
+
+
+    def match(self,words):
+            anagrams=[]
+            for word in words:
+                if sorted(word.lower())==sorted(self.anagram.lower()) and word.lower() != self.anagram.lower():
+                    anagrams.append(word)
+            return anagrams
+ 
+               
+
+
+
+
+
+listen=Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
